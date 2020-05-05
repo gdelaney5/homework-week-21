83fcf4a0f1f622463274f43c2540b3c901f8d2a9
Garrett Delaney <gdelaney@wep-inc.com>
2020-05-05 11:48:13 -0700

    found issues in server file

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes)
 
 //Connect to MongoDB
-mongoose.connect(process.env.MONGOD_URI || "mongodb://mongodb://user1:password1@ds059215.mlab.com:59215/heroku_pskcb7m3");
+mongoose.connect(process.env.MONGOD_URI || "mongodb://user123:password123@ds059215.mlab.com:59215/heroku_pskcb7m3");
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
